78b540d8a691d7e3d88962228d5802b25656baec
immanuelcherui1 <mmanuelcherui1@gmail.com>
2024-02-14 15:46:52 +0300

    Initial paypal options

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
-// import './App.css'
+import Integration from './Intergration'
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
+import './App.css'
+
+
+const initialOptions = {
+  "client-id": "Ae5gS9dNR_4ZF2Eat_BC2Tj7ZyeSayZ3KJ8vHdEtS8qcTrLuvDJbhEW6DiqKMm1wVjy6TjDgfvsurdBQ",
+  currency: "USD",
+  intent: "capture",
+};
 
 function App() {
   
@@ -9,6 +18,12 @@ function App() {
   return (
     <>
    <h1>Hello Team B.</h1> 
+   <Integration/>
+
+   <PayPalScriptProvider options={initialOptions}>
+        <Checkout/>
+</PayPalScriptProvider>
+
     </>
   )
 }
