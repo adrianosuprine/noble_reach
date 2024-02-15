a53dddf2a4fe107eba6d6f531f96e9ff12ffad21
adrianosuprine <adrianosuprine@gmail.com>
2024-02-15 16:55:48 +0300

    programcard design

--- src/Components/main.jsx
+++ src/Components/main.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 // import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
 
 import { BrowserRouter } from 'react-router-dom'
+import ProgramsandEventsPage from './ProgramsandEventsPage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter/>
-    <App />
+    <ProgramsandEventsPage />
     <BrowserRouter/>
   </React.StrictMode>,
 )
