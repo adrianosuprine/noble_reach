db3b26c764cc40100f9081f52b162c6f9e2dbfe1
adrianosuprine <adrianosuprine@gmail.com>
2024-02-16 00:18:23 +0300

    styling the calender

--- src/Components/EventsPage.jsx
+++ src/Components/EventsPage.jsx
@@ -1,24 +1,29 @@
 import { useState, useEffect } from 'react';
 import Calendar from 'react-calendar';
-import './EventsPage.css'; // Add your custom styles
+import './EventsPage.css';
 
 const EventsPage = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [events, setEvents] = useState([]);
+  const [filteredEvents, setFilteredEvents] = useState([]);
 
   useEffect(() => {
-    // const formattedDate = selectedDate.toISOString().split('T')[0];
-
     fetch(`http://localhost:3000/events`)
       .then((res) => res.json())
       .then((data) => setEvents(data))
       .catch((error) => console.error(error));
-  }, [selectedDate]);
+  }, []);
+
+  useEffect(() => {
+    // Filter events based on the selected date
+    const formattedDate = selectedDate.toISOString().split('T')[0];
+    const filtered = events.filter((event) => event.date === formattedDate);
+    setFilteredEvents(filtered);
+  }, [selectedDate, events]);
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
-  console.log(events);
 
   return (
     <div className="events-container">
@@ -27,17 +32,30 @@ const EventsPage = () => {
       </div>
 
       <div className="events-list">
-        <h2>Events for {selectedDate.toDateString()}</h2>
-        <ul>
-          {events.map((event) => (
-            <li key={event.id}>
-              <h3>{event.title}</h3>
-              <p>{event.description}</p>
-              <p>Date: {event.date}</p>
-              <p>Location: {event.location}</p>
-            </li>
-          ))}
-        </ul>
+        {/* <h2>Events for {selectedDate.toDateString()}</h2> */}
+        {filteredEvents.length === 0 ? (
+         <ul>
+         {events.map((event) => (
+           <li key={event.id}>
+             <h3>{event.title}</h3>
+             <p>{event.description}</p>
+             <p>Date: {event.date}</p>
+             <p>Location: {event.location}</p>
+           </li>
+         ))}
+       </ul>
+        ) : (
+          <ul className='event'>
+            {filteredEvents.map((event) => (
+              <li key={event.id}>
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <p> <span> Date: {event.date}</span> </p>
+                <p> <span> Location: {event.location} </span> </p>
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
--- src/Components/ProgramsPage.jsx
+++ src/Components/ProgramsPage.jsx
@@ -1,6 +1,6 @@
 import { useState,useEffect } from "react";
 import ProgramsCard from "./ProgramsCard";
-import './App.css';
+import './Page.css';
 
 function ProgramsPage(){
     const [programs, setPrograms] = useState([]);
