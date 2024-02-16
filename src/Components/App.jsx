3f9e079e9cf18cbf386db3214318f305ef9e636e
Ian <iankosgey535@gmail.com>
2024-02-16 12:22:04 +0300

    resolved merge conflicts

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,19 +1,25 @@
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
+
 import Integration from "./Intergration";
 import LandingPage from "./LandingPage";
-import Footer from "./Footer";
+import EventsPage from "./EventsPage";
+import ProgramsPage from "./ProgramsPage";
 import Navbar from "./Navbar";
+import Footer from "./Footer";
+import VolunteerForm from "./VolunteerForm";
 
 function App() {
   // All routes placed here.
 
   return (
     <>
-     <Navbar />
+       <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/programs" element={<ProgramsPage />} />
         <Route path="/donate" element={<Integration />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/volunteers" element={ <VolunteerForm/>} />
       </Routes>
       <Footer />
     </>
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,21 +1,24 @@
-import React from "react";
+// import React from "react";
+// import { Routes, Route } from "react-router-dom";
+
+
 import Cards from "./Cards";
-import Gallery from "./Gallery";
 
 function LandingPage() {
   return (
     <div>
-     
      
       <div>
         <h1 className="text-blue-700 font-bold  text-normal">
           Welcome to Noble Reach
         </h1>
       </div>
+
       <section className="flex">
         <div className="max-w-medium bg-white border border-black-200 rounded-lg flex">
           <div className="p-5 w-1/2">
             {" "}
+            
             <p className="mb-3 font-normal text-black-700">
               At Noble Reach, we believe in the power of compassion to make a
               difference. Our mission is to create positive change by supporting
@@ -24,12 +27,13 @@ function LandingPage() {
             </p>
           </div>
           <img
-            className="rounded-t-lg w-1/2"
+            className="rounded-t-lg w-1/2" 
             src="/src/assets/dispicon.jpeg"
             alt="charity image"
           />
         </div>
       </section>
+
       <section>
         <div className="justify-center pdl-45">
           <a
@@ -40,9 +44,12 @@ function LandingPage() {
           </a>
         </div>
       </section>
-      <Cards />
-      <Gallery />
-      
+
+      <section>
+        <Cards />
+      </section>
+
+     
     </div>
   );
 }
