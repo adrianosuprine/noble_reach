55b7d471782e91abc7f3c845b921d1e1c92f27a9
Ian <iankosgey535@gmail.com>
2024-02-16 10:27:45 +0300

    Gallery ft added

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -2,16 +2,20 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import Integration from "./Intergration";
 import LandingPage from "./LandingPage";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 function App() {
   // All routes placed here.
 
   return (
     <>
+     <Navbar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/donate" element={<Integration />} />
       </Routes>
+      <Footer />
     </>
   );
 }
--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,24 +1,23 @@
 // import React from "react";
 // import { Routes, Route } from "react-router-dom";
-import Footer from "./Footer";
-import Navbar from "./Navbar";
+
+
 import Cards from "./Cards";
+import Gallery from "./Gallery";
 
 function LandingPage() {
   return (
     <div>
-      <Navbar />
+     
       <div>
         <h1 className="text-blue-700 font-bold  text-normal">
           Welcome to Noble Reach
         </h1>
       </div>
-
       <section className="flex">
         <div className="max-w-medium bg-white border border-black-200 rounded-lg flex">
           <div className="p-5 w-1/2">
             {" "}
-            
             <p className="mb-3 font-normal text-black-700">
               At Noble Reach, we believe in the power of compassion to make a
               difference. Our mission is to create positive change by supporting
@@ -27,13 +26,12 @@ function LandingPage() {
             </p>
           </div>
           <img
-            className="rounded-t-lg w-1/2" 
+            className="rounded-t-lg w-1/2"
             src="/src/assets/dispicon.jpeg"
             alt="charity image"
           />
         </div>
       </section>
-
       <section>
         <div className="justify-center pdl-45">
           <a
@@ -44,12 +42,9 @@ function LandingPage() {
           </a>
         </div>
       </section>
-
-      <section>
-        <Cards />
-      </section>
-
-      <Footer />
+      <Cards />
+      <Gallery />
+      
     </div>
   );
 }
